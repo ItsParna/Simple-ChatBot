fb0cb1070c5b0d3fd9cf47bc087b01752b1b93c8
ParnaC <parnachakrabarti.2004@gmail.com>
2025-01-22 23:47:21 +0530

    update

---
+++ src/app/config.js
@@ -0,0 +1,13 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  botName: "ChatNer",
+  initialMessages: [
+    createChatBotMessage(`Hello I am ChatNer!! How can I help You?`)
+  ]
+}
+
+export default config
+
+
+// bot Avatar
